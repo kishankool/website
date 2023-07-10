85729b2c006cdb035c2a3f79411d240abb298022
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-07-10 15:56:27 +0530

    added new website

---
+++ dist/js/base.js
@@ -0,0 +1,5 @@
+var homeWeb = document.querySelector('.home-web');
+function hello(compiler) {
+    console.log("Hello from ".concat(compiler));
+}
+hello("TypeScript");
---
+++ src/ts/base.ts
@@ -0,0 +1,5 @@
+const homeWeb=document.querySelector('.home-web');
+function hello(compiler: string) {
+    console.log(`Hello from ${compiler}`);
+  }
+  hello("TypeScript");  
